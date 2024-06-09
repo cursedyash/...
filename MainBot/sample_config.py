9887883eb60002e5d2f7cd54b298581baeda458a
cursedyash <apatheticyash@gmail.com>
2024-06-09 15:53:19 +0530

    Update sample_config.py

--- MainBot/sample_config.py
+++ MainBot/sample_config.py
@@ -1,5 +1,22 @@
 class Config(object):
-    pass
+        TOKEN = (
+        ""  # bot token from bot >
+    )
+    BOT_USERNAME = ""  # bot username
+    OWNER_ID = ""  # Owner account user id
+    OWNER_USERNAME = ""  # Owner account username
+    SQLALCHEMY_DATABASE_URI = ""
+    ADMINS = set(["1439206175"])
+    ERROR_LOGS = -1002096520973  # error logging group id here
+    FIRST_NAMES = {}
+    CURRENT_USERS = set([])
+    EVENT_LOGS = -1002096520973  # new joinee logger id
+    MAIN_CHANNEL = -1002109329180  # database channel id
+    FORCE_SUB = {"id": ""}  # force sub channel default id
+    BATCH_FILES = {"status": False}
+    PROTECT_CONTENT = True
+
+
 
 
 class Production(Config):
